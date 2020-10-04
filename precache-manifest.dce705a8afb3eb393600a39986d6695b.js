fcf1ee4e55c7c535b8ffa98821858bd786c62851
dekpient <dekpient@users.noreply.github.com>
2020-10-04 00:46:38 +0000

    deploy: 3b8a3b9198d881811412bca5b8198d7afbc5c8d7

---
+++ precache-manifest.dce705a8afb3eb393600a39986d6695b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a6c7e1563360d3ffb13e8d6d2622d6",
+    "url": "/vegetarian-menu-calculator/index.html"
+  },
+  {
+    "revision": "9514d2b89353c733095a",
+    "url": "/vegetarian-menu-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c3356df5e5a2cb5fdee",
+    "url": "/vegetarian-menu-calculator/static/js/2.2571d2ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vegetarian-menu-calculator/static/js/2.2571d2ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9514d2b89353c733095a",
+    "url": "/vegetarian-menu-calculator/static/js/main.4588a23f.chunk.js"
+  },
+  {
+    "revision": "619662534df8572127f3",
+    "url": "/vegetarian-menu-calculator/static/js/runtime-main.44c9bea2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vegetarian-menu-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
