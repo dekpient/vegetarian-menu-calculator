18634ff4943dad9d132a37a6f928bb6c8913fe51
Nitkalya Wiriyanuparb <dekpient@gmail.com>
2020-10-04 16:38:03 +1300

    Add lodash and fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import _ from 'lodash';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders days 0-11', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const elements = _.times(11, day => getByText(`Day ${day}`));
+  _.forEach(elements, element => expect(element).toBeInTheDocument());
 });
